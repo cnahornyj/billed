e216c6c4f0e79198cbf2cdbaf10aeda16db21c07
Clara Nahornyj <c.nahornyj@hotmail.fr>
2021-07-12 14:58:26 +0200

    Possibilité de déplier plusieurs listes, et consulter les tickets de chacune des deux listes

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -88,7 +88,7 @@ export default class {
   handleEditTicket(e, bill, bills) {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
+    if (this.counter % 2 === 0 || this.counter % 2 === 1) {
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
